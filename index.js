ee6e34e5d0f87acfad3311a8374bce628d016a3a
Alexandra Johnson <alexandra@queryclick.com>
2018-10-30 19:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver={};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  driver[key]=value;
+  return driver 
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  
+  return driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  
+  return driver;
+}
